698d9bbd9933c19ae2d950577da45bb599fb1982
Zxt34 <3265923652@qq.com>
2021-03-09 19:35:16 +0800

    笔试题

--- Exception/NameException.java
+++
@@ -1,7 +0,0 @@
-package Exception;
-
-public class NameException extends  Exception {
-    public NameException(String message) {
-        super(message);
-    }
-}
--- Exception/PasswordException.java
+++
@@ -1,7 +0,0 @@
-package Exception;
-
-public class PasswordException extends Exception {
-    public PasswordException(String message) {
-        super(message);
-    }
-}
