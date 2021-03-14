cf2292fb78fac9071d68aa2f429ea5dd5c518e00
Zxt34 <3265923652@qq.com>
2021-03-14 22:37:40 +0800

    判断两个树是否相同

---
+++ Test3_14_1.java
@@ -0,0 +1,23 @@
+public class Test3_14_1 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
