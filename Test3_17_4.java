f0bfe8824bfc0d00b2d5a1849bde0edc065a0d62
Zxt34 <3265923652@qq.com>
2021-03-16 23:15:51 +0800

    二叉树笔试题3

---
+++ Test3_17_4.java
@@ -0,0 +1,10 @@
+public class Test3_17_4 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+    }
+}
